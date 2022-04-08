fae40f2841216c2696a0585dd255e0151e681377
PovilasKorop <povilask007@gmail.com>
2022-04-08 09:18:56 +0300

    Lesson 3 - static homepage

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/second', function () {
-    return view('second');
-});
